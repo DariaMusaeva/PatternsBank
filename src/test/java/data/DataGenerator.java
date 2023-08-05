7440bc82f6dac10055207b0f56f7d8e13d275f20
Mel66292 <mel57792@yandex.ru>
2023-08-05 19:29:54 +0500

    New tests added

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -2,6 +2,7 @@ package data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -17,9 +18,19 @@ public class DataGenerator {
     }
 
     public static String generateCity() {
-        var cities = new String[]{"Москва", "Пермь", "Санкт-Петербург", "Сочи", "Архангельск", "Саратов",
+        var cities = new String[]{"Москва", "Пермь", "Санкт-Петербург", "Архангельск", "Саратов",
                 "Омск", "Тверь", "Тюмень", "Курск", "Казань", "Екатеринбург", "Челябинск", "Ижевск"};
         return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateInvalidCity() {
+        var invalidCities = new String[]{"1234", "Сочи", "??:;;г", "Sochi"};
+        return invalidCities[new Random().nextInt(invalidCities.length)];
+    }
+
+    public static String generateEmptyCity() {
+        String emptyCity = new String();
+        return emptyCity;
     }
 
     public static String generateName(String locale) {
@@ -27,23 +38,61 @@ public class DataGenerator {
         return faker.name().fullName();
     }
 
+    public static String generateInvalidName() {
+        var faker = new Faker();
+        return faker.funnyName().name();
+    }
+
+    public static String generateEmptyName() {
+        String emptyName = new String();
+        return emptyName;
+    }
+
     public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
 
+    public static String generateEmptyPhone() {
+        String emptyPhone = new String();
+        return emptyPhone;
+    }
+
     public static class Registration {
-        private Registration() {}
+        private Registration() {
+        }
 
         public static UserInfo generateUser(String locale) {
             return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+
+        public static InvalidUser generateInvalidUser(String locale) {
+            return new InvalidUser(generateInvalidCity(), generateInvalidName(), generatePhone(locale));
+        }
+
+        public static EmptyUser generateEmptyUser() {
+            return new EmptyUser(generateEmptyCity(), generateEmptyName(), generateEmptyPhone());
         }
     }
 
     @Value
     public static class UserInfo {
+        String city;
         String name;
         String phone;
-        String city;
+    }
+
+    @Value
+    public static class InvalidUser {
+        String invalidCity;
+        String funnyName;
+        String phone;
+    }
+
+    @Value
+    public static class EmptyUser {
+        String emptyCity;
+        String emptyName;
+        String emptyPhone;
     }
 }
--- src/test/java/test/PatternsBankTest.java
+++ src/test/java/test/PatternsBankTest.java
@@ -1,18 +1,23 @@
 package test;
 
+import com.codeborne.selenide.Condition;
 import data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PatternsBankTest {
 
     @BeforeEach
-        void setUp() {
+    void setUp() {
         open("http://localhost:9999");
     }
 
@@ -30,5 +35,149 @@ public class PatternsBankTest {
         $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(5));
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать? "))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+        $(byText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("shouldTestFormWithoutAgreement")
+    void shouldTestFormWithoutAgreement() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $(byText("Запланировать")).click();
+        $("[data-test-id=agreement] .checkbox__text")
+                .shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestInvalidName")
+    void shouldTestInvalidName() {
+        DataGenerator.InvalidUser invalidUser = DataGenerator.Registration.generateInvalidUser("eng");
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(invalidUser.getFunnyName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='name'] .input__sub")
+                .shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestInvalidCity")
+    void shouldTestInvalidCity() {
+        DataGenerator.InvalidUser invalidUser = DataGenerator.Registration.generateInvalidUser("eng");
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(invalidUser.getInvalidCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='city'] .input__sub")
+                .shouldHave(text("Доставка в выбранный город недоступна"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestEmptyCity")
+    void shouldTestEmptyCity() {
+        DataGenerator.EmptyUser emptyUser = DataGenerator.Registration.generateEmptyUser();
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(emptyUser.getEmptyCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='city'] .input__sub")
+                .shouldHave(text("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestEmptyDate")
+    void shouldTestEmptyDate() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='date'] .input__sub")
+                .shouldHave(text("Неверно введена дата"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestEmptyName")
+    void shouldTestEmptyName() {
+        DataGenerator.EmptyUser emptyUser = DataGenerator.Registration.generateEmptyUser();
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(emptyUser.getEmptyName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='name'] .input__sub")
+                .shouldHave(text("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("shouldTestEmptyPhone")
+    void shouldTestEmptyPhone() {
+        DataGenerator.EmptyUser emptyUser = DataGenerator.Registration.generateEmptyUser();
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(emptyUser.getEmptyPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='phone'] .input__sub")
+                .shouldHave(text("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
     }
 }
